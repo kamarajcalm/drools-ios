3c7af48c069fee19f378dc86e9b02eca6ae55116
kamaraj <kamraj089@gmail.com>
2021-09-04 15:16:59 +0530

    fww

--- stacks/ProfileStack.js
+++ stacks/ProfileStack.js
@@ -8,14 +8,16 @@ import StatisticsView from '../screens/StatisticsView';
 import ProfilePage from '../screens/ProfilePage';
 import Tables from '../screens/Tables';
 import ExpenseScreen from '../screens/ExpenseScreen';
-
+import BlueToothDevices from '../screens/BlueToothDevices';
 import { TransitionSpecs, CardStyleInterpolators } from '@react-navigation/stack';
 import Discounts from '../screens/Discounts';
 import OtherExpenses from '../screens/OtherExpenses';
 import PasswordScreen from '../customerScreens/PasswordScreen';
 import ProfileInfo from '../screens/ProfileInfo';
+import PrintBills from '../screens/PrintBills';
 import MonthlyIncome from '../screens/MonthlyIncome';
 import DailyIncomeSheet from '../screens/DailyIncomeSheet';
+import DailyExpenses from '../screens/DailyExpenses';
 const Stack = createStackNavigator();
 export default class ProfileStack extends Component {
     constructor(props) {
@@ -42,11 +44,14 @@ export default class ProfileStack extends Component {
                 <Stack.Screen name="Tables" component={Tables} options={{ headerShown: false }} />
                 <Stack.Screen name="ExpenseScreen" component={ExpenseScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="OtherExpenses" component={OtherExpenses} options={{ headerShown: false }} />
+                <Stack.Screen name="BlueToothDevices" component={BlueToothDevices} options={{ headerShown: false }} />
                 <Stack.Screen name="Discounts" component={Discounts} options={{ headerShown: false }} />
                 <Stack.Screen name="PasswordScreen" component={PasswordScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="ProfileInfo" component={ProfileInfo} options={{ headerShown: false }} />
+                <Stack.Screen name="PrintBills" component={PrintBills} options={{ headerShown: false }} />
                 <Stack.Screen name="MonthlyIncome" component={MonthlyIncome} options={{ headerShown: false }} />
                 <Stack.Screen name="DailyIncomeSheet" component={DailyIncomeSheet} options={{ headerShown: false }} />
+                <Stack.Screen name="DailyExpenses" component={DailyExpenses} options={{ headerShown: false }} />
             </Stack.Navigator>
         );
     }
