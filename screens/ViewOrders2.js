358e484f4504187b9fd8244de7a28c8ea98cb4c8
Akhil <kamraj089@gmail.com>
2021-09-23 12:34:18 +0530

    dada

--- screens/ViewOrders2.js
+++ screens/ViewOrders2.js
@@ -47,25 +47,22 @@ const paymentMode = [
         label: "Cash",
         value: "Cash"
     },
-    {
-        label: "Card",
-        value: "Card"
-    },
-    {
-        label: "OtherUpis",
-        value: "OtherUpis"
-    },
-    {
-        label: "Paytm",
-        value: "Paytm"
-    },
-    {
-        label: "Gpay",
-        value: "Gpay"
-    },
+
     {
         label: "Phonepe",
         value: "Phonepe"
+    },
+    {
+        label: "Online",
+        value: "Online"
+    },
+    {
+        label: "Personal1",
+        value: "Personal1"
+    },
+    {
+        label: "Personal2",
+        value: "Personal2"
     },
 ]
 
